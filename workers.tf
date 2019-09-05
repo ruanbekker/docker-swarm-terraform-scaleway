fb216f36d855b40d44f982e28333d11fc2b25958
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-09-05 08:19:19 +0200

    Create workers.tf

---
+++ workers.tf
@@ -0,0 +1,111 @@
+resource "scaleway_ip" "swarm_worker_ip" {
+  count = "${var.worker_instance_count}"
+}
+
+resource "scaleway_server" "swarm_worker" {
+  count          = "${var.worker_instance_count}"
+  name           = "${terraform.workspace}-worker-${count.index + 1}"
+  image          = "${data.scaleway_image.debian_stretch.id}"
+  type           = "${var.worker_instance_type}"
+  bootscript     = "${data.scaleway_bootscript.debian.id}"
+  security_group = "${scaleway_security_group.swarm_workers.id}"
+  public_ip      = "${element(scaleway_ip.swarm_worker_ip.*.ip, count.index)}"
+
+  volume {
+    size_in_gb = 50
+    type       = "l_ssd"
+  }
+
+  provisioner "remote-exec" {
+    script = "scripts/mount-disk.sh"
+  }
+
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /etc/systemd/system/docker.service.d",
+    ]
+  }
+
+  provisioner "file" {
+    content     = "${data.template_file.docker_conf.rendered}"
+    destination = "/etc/systemd/system/docker.service.d/docker.conf"
+  }
+
+  provisioner "file" {
+    source      = "scripts/install-docker-ce.sh"
+    destination = "/tmp/install-docker-ce.sh"
+  }
+
+  provisioner "file" {
+    source      = "scripts/local-persist-plugin.sh"
+    destination = "/tmp/local-persist-plugin.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/install-docker-ce.sh",
+      "/tmp/install-docker-ce.sh ${var.docker_version}",
+      "docker swarm join --token ${data.external.swarm_tokens.result.worker} ${scaleway_server.swarm_manager.0.private_ip}:2377",
+      "chmod +x /tmp/local-persist-plugin.sh",
+      "/tmp/local-persist-plugin.sh",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    when = "destroy"
+
+    inline = [
+      "docker node update --availability drain ${self.name}",
+    ]
+
+    on_failure = "continue"
+
+    connection {
+      type = "ssh"
+      user = "root"
+      host = "${scaleway_ip.swarm_manager_ip.0.ip}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    when = "destroy"
+
+    inline = [
+      "docker swarm leave",
+    ]
+
+    on_failure = "continue"
+  }
+
+  provisioner "remote-exec" {
+    when = "destroy"
+
+    inline = [
+      "docker node rm --force ${self.name}",
+    ]
+
+    on_failure = "continue"
+
+    connection {
+      type = "ssh"
+      user = "root"
+      host = "${scaleway_ip.swarm_manager_ip.0.ip}"
+    }
+  }
+}
+
+data "external" "swarm_tokens" {
+  program = ["./scripts/fetch-tokens.sh"]
+
+  query = {
+    host = "${scaleway_ip.swarm_manager_ip.0.ip}"
+  }
+
+  depends_on = ["scaleway_server.swarm_manager"]
+}
